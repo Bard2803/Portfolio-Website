b742055b83e031d4559bbfbdb4735172bdd460c1
Bard2803 <palusba@gmail.com>
2022-09-23 00:39:50 +0200

    modifies Cipher folder and Cypher.py file to adjust it for the website. Builds the cipher function in app.py to handle the cipher file.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.security import check_password_hash, generate_password_hash
+import Cipher.Cipher as cr
+
+### TO RUN FLASK
+### python -m flask run 
 
 # Confugre the application
 app = Flask(__name__)
@@ -17,4 +19,16 @@ def index():
 def cipher():
     ''' Cipher project'''
 
-    return render_template("cipher.html")
+    if request.method == "GET":
+        return render_template("cipher.html")
+    else:
+        # Get the message as str from cipher.html
+        message = request.form.get("encrypted_message")
+
+        # Get the shift as int from ciper.html
+        shift = int(request.form.get("shift"))
+
+        # Use the encryption from Cipher file 
+        encrypted_message = cr.PlaintextMessage(message, shift).get_message_text_encrypted()
+
+        return render_template("cipher.html", encrypted_message=encrypted_message)
